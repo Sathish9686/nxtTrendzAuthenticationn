3c6567c2be3000baff0e4684dfe083b0a5dc7860
Sathish9686 <princesathish9686@gmail.com>
2023-04-10 05:41:09 +0000

    nxtTrendzAuthentication

---
+++ src/components/Cart/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Cart = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,13 @@
+import './index.css'
+
+const Product = () => (
+  <div className="not-found-container">
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png "
+      alt=" products"
+      className="not-found-img"
+    />
+  </div>
+)
+
+export default Product
